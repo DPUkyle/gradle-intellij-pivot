8751972209ced7bd0144a093c53f8413fbad2290
Kyle Moore <github@kylemoore.com>
2017-12-21 11:13:05 -0800

    Verify source of LightCodeInsightFixtureTestCase

--- test/src/test/java/FooTest.java
+++ test/src/test/java/FooTest.java
@@ -1,9 +1,11 @@
-import org.junit.Test;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 
-public class FooTest {
+public class FooTest extends LightCodeInsightFixtureTestCase {
   
-  @Test
-  public void uCanHazFoo() {
-    //Foo foo = new Foo(); //actually, we won't depend on plugin's artifact, so Foo is out of scope 
+  public void testPrintSuperclassCodeLocation() {
+    //Foo foo = new Foo(); //actually, we won't depend on plugin's artifact, so Foo is out of scope
+    
+    //I don't trust that the sandbox is using the pivoted version of IJ. Print the test superclass's code source to make sure it's using the expected IJ API JAR
+    System.out.println("LightCodeInsightFixtureTestCase sourced from: " + LightCodeInsightFixtureTestCase.class.getProtectionDomain().getCodeSource().getLocation() );
   }
 }
